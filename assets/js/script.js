f2396ef0c9ff5f230c2f57a98cec892d76a185a8
Ricardo Araujo <ricardoasf@id.uff.br>
2025-04-23 16:24:32 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
+    [...popoverTriggerList].forEach(el => {
+        new bootstrap.Popover(el);
+    });
+});
